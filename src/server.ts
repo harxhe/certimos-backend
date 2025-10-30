70085001e5a0267ff03e3e8e929033feffa54fa0
Harsh Vardhan <harshvardhan.p27@gmail.com>
2025-10-30 22:02:22 +0530

    Added deployed frontend to cors
    Added a new allowed origin for CORS.

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:3001'
+    'http://127.0.0.1:3001',
+    'https://certimos.vercel.app'
   ],
   credentials: true
 }));
